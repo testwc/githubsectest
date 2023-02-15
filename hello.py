d1b97d34d68b23d864b7bf07d8a3b843741b915a
王川 <wangchuan7@xiaomi.com>
2023-02-15 14:52:08 +0800

    update

---
+++ hello.py
@@ -0,0 +1,43 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def hello():
+    url="http://relay.xiaomi.com"
+    username="root"
+    password="hellogithubtest"
+    access_key="3454534534343"
+    access_secret="144cd6b654c6425450c79b3c720ad465"
+    result={
+        "url":url,
+        "username":username,
+        "password":password,
+        "access_key":access_key,
+        "access_secret":access_secret
+    }
+    return result
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('hello@xiaomi.com')
+    print_hi('password=123456')
+    print_hi('accesskey=ewqewtgdagsdfadsfaewrwe32rff')
+    print_hi('url=jdbc:mysql')
+    print_hi('url="b2c.srv')
+    print_hi('url=relay.xiaomi.com')
+    print_hi('url=hello.mioffice.cn')
+    print_hi('url=')
+    print_hi('url=www.mioffice.cn')
+    print_hi('mail=nani@xiaomi.com')
+    print_hi('password=Wtf?yfd?')
+    hello()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
